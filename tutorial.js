c98658c4b3a25d08ff5655a77820c00fd275c15b
pagetribe <pagetribemail@gmail.com>
2018-03-20 21:38:32 +1100

    got search working

--- tutorial.js
+++ tutorial.js
@@ -2,12 +2,28 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load()
 }
 const { IncomingWebhook, WebClient } = require('@slack/client')
+console.log('Getting started...')
+
+const web = new WebClient(process.env.SLACK_TOKEN)
 const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
 const currentTime = new Date().toTimeString()
+
 timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
     if(error) {
         return console.error(error)
     }
     console.log('Notification Sent')
+    console.log('Waiting a few seconds for search indexes to update...')
+    setTimeout(() => {
+        console.log('Calling search.messages')
+        web.search.messages({query: currentTime })
+            .then(resp => {
+                if(resp.messages.total > 0) {
+                    console.log('First match: ', resp.messages.matches[0])
+                } else {
+                    console.log('No matches found')
+                }
+            })
+            .catch(console.error)
+    }, 200000)
 })
-console.log('Getting started...')
