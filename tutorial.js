97d44e627f275b8527d4d07fc730804827bd8710
pagetribe <pagetribemail@gmail.com>
2018-03-20 20:49:47 +1100

    sending time stamp message working

--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load()
+}
 const { IncomingWebhook, WebClient } = require('@slack/client')
-console.log('Getting started')
+const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+const currentTime = new Date().toTimeString()
+timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
+    if(error) {
+        return console.error(error)
+    }
+    console.log('Notification Sent')
+})
+console.log('Getting started...')
